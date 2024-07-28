b71ee73c53ac4b53794e953658a6f834180feae2
Michael Serwin.S <97592663+michaelserwin@users.noreply.github.com>
2024-07-28 22:34:13 +0530

    print 1 to n numbers

---
+++ printnums/src/Printnums.java
@@ -0,0 +1,25 @@
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Micha
+ */
+
+public class Printnums {
+    public static void main (String []args)
+    {
+        java.util.Scanner sc= new java.util.Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n=sc.nextInt();
+        for (int i=1;i<=n;i++){
+            System.out.println(i);
+        }
+    }
+    
+}
